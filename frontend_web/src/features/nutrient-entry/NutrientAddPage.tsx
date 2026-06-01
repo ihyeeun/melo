7ec4077fe9f46cb26a0dd9fd8762d3d13c7a7a19
ihyeeun <oioigl@naver.com>
2026-06-01 23:34:58 +0900

    fix: input length 제한

--- frontend_web/src/features/nutrient-entry/NutrientAddPage.tsx
+++ frontend_web/src/features/nutrient-entry/NutrientAddPage.tsx
@@ -66,7 +66,7 @@ export default function NutrientAddPage() {
   }, [brandSearchReturnKey, clearBrandSearchSelection]);
 
   const handleFoodNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setFoodName(event.target.value);
+    setFoodName(event.target.value.slice(0, 300));
   };
 
   const handleOpenBrandSearch = () => {
