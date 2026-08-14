6038a8e76b0008ebcd7e8b9b86f0286782c61cac
ihyeeun <oioigl@naver.com>
2026-08-14 14:45:24 +0900

    chore: 스타일 린트 의존성 제거

--- frontend_web/stylelint.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["stylelint-config-standard"],
-  plugins: ["stylelint-order"],
-  rules: {
-    "order/properties-alphabetical-order": true,
-    "selector-class-pattern": null,
-  },
-};
